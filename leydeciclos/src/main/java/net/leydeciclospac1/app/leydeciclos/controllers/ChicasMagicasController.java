ecbca453a18e4c6fea4a174ede784f1bc649d6fb
Tiaguitoo09 <martinezbeltransantiago9@gmail.com>
2025-02-24 14:36:11 -0500

    Cambios en Controlador para el uso de los endpoints en la pagina

--- leydeciclos/src/main/java/net/leydeciclospac1/app/leydeciclos/controllers/ChicasMagicasController.java
+++ leydeciclos/src/main/java/net/leydeciclospac1/app/leydeciclos/controllers/ChicasMagicasController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import net.leydeciclospac1.app.leydeciclos.entities.ChicasMagicas;
 import net.leydeciclospac1.app.leydeciclos.services.ChicasMagicasService;
 
+@CrossOrigin(origins = "http://localhost:8000") 
 @RestController
 @RequestMapping("/chicas-magicas")
 public class ChicasMagicasController {
